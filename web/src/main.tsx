3f473cd779ac2bbf7ae91dcf5314e2b9980aecb0
Andre Sales <asalesg@gmail.com>
2022-05-06 19:47:05 -0300

    update botão

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     
     <h1 className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center'>Esse "botãozinho" é meu primeiro projeto feito em React com Typescript.</h1>
     <p className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center'>Foram muitos erros até conseguir deixar 100% funcional. Agora ele tem um servidor onde recebo os feedbacks e até a imagem.</p>
-    <button className='text-xs m-5 p-2 white rounded-md border border-violet-500 font-bold flex justify-center items-center center'><a target="_blank" href="https://asalesg.github.io/portfolio/">Clique aqui para ter mais informações minhas</a></button>
+    <button className='text-xs m-5 p-2 white rounded-md border border-violet-500 font-bold flex justify-center items-center center'><a target="_blank" href="https://asalesg.github.io/portfolio/">Clique para acessar uma página com mais informações sobre mim.</a></button>
     <App />
   </React.StrictMode>
 )
