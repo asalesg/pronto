f040625e65e9b73d7138f9216ddbdd2904f9d5b6
Andre Sales <asalesg@gmail.com>
2022-05-06 19:17:01 -0300

    add botao

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,9 +8,9 @@ import './global.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     
-    <h1 className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center center'>Esse "botãozinho" é meu primeiro projeto feito em React com Typescript.</h1>
-    <p className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center center'>Foram muitos erros até conseguir deixar 100% funcional. Agora ele tem um servidor onde recebo os feedbacks e até a imagem.</p>
-    
+    <h1 className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center'>Esse "botãozinho" é meu primeiro projeto feito em React com Typescript.</h1>
+    <p className='m-5 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center'>Foram muitos erros até conseguir deixar 100% funcional. Agora ele tem um servidor onde recebo os feedbacks e até a imagem.</p>
+    <button className='text-xs m-5 p-2 white rounded-md border border-violet-500 font-bold flex justify-center items-center center'><a target="_blank" href="https://asalesg.github.io/portfolio/">Clique aqui para ter mais informações minhas</a></button>
     <App />
   </React.StrictMode>
 )
